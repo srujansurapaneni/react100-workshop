96309ba9338fabe6ba6af6a56cca1f671612fd75
srujansurapaneni <srujansurapaneni@gmail.com>
2019-12-29 20:33:22 -0500

    1. props value to square

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -5,7 +5,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.dis}
       </button>
     );
   }
@@ -13,7 +13,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square dis={'X'}/>;
   }
 
   render() {
